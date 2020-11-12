0e199869e8bda14e5f3f8e921103d9fc9c4db26a
maka y <nord2sudjp@gmail.com>
2020-11-12 23:04:06 +0900

    Add new transaction successfully

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
+  final Function _addTx;
+
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  NewTransaction(this._addTx);
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -30,8 +35,10 @@ class NewTransaction extends StatelessWidget {
               child: Text('Add Transaction'),
               textColor: Colors.purple,
               onPressed: () {
-                print(titleController.text);
-                print(amountController.text);
+                //print(titleController.text);
+                //print(amountController.text);
+                _addTx(
+                    titleController.text, double.parse(amountController.text));
               },
             ),
           ],
