4867d021d349a9f7b09644ed2226dccee583d6dd
maka y <nord2sudjp@gmail.com>
2020-11-13 14:05:06 +0900

    SingleChildScrollView

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,42 +8,48 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-            child: Row(children: [
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.purple, width: 2),
-            ),
-            padding: EdgeInsets.all(10),
-            child: Text(
-              '\$${tx.amount}',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.purple),
-            ),
-          ),
-          Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  tx.title,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((tx) {
+            return Card(
+                child: Row(children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.purple, width: 2),
                 ),
-                Text(
-                  DateFormat('yyyy/MM/dd').format(tx.date),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  '\$${tx.amount}',
                   style: TextStyle(
-                      fontSize: 14,
                       fontWeight: FontWeight.bold,
-                      color: Colors.grey),
+                      fontSize: 20,
+                      color: Colors.purple),
                 ),
-              ])
-        ]));
-      }).toList(),
+              ),
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tx.title,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat('yyyy/MM/dd').format(tx.date),
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    ),
+                  ])
+            ]));
+          }).toList(),
+        ),
+      ),
     );
   }
 }
