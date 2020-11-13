760718eab98a12c6254097442982a687c63538e9
maka y <nord2sudjp@gmail.com>
2020-11-13 22:58:25 +0900

    Implement barchart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../model/transaction.dart';
 import 'package:intl/intl.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> rT;
@@ -27,6 +28,12 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -34,7 +41,9 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Row(
         children: groupedTransactionValues.map((data) {
-          return Text('${data['day']} : ${data['amount']}');
+          //return Text('${data['day']} : ${data['amount']}');
+          return ChartBar(data['day'], data['amount'],
+              (data['amount'] as double) / totalSpending);
         }).toList(),
       ),
     );
